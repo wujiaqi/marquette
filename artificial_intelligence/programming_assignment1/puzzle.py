53145c454fdbf3452c3ca9fed73573186bc36920
Jiaqi Wu <wujiaqi@Jiaqis-MacBook-Pro.local>
2013-09-18 00:14:01 -0700

    working solution

--- artificial_intelligence/programming_assignment1/puzzle.py
+++ artificial_intelligence/programming_assignment1/puzzle.py
@@ -1,14 +1,17 @@
+import math
+
 #Node class which defines the state of the game
 class Node:
-	def __init__(self, state, parent = None):
+	def __init__(self, state, parent = None, cost = 0):
 		self.state = state
 		self.parent = parent
+		self.cost = cost
 
 	def __makeChildNode(self, swap_index, empty_index):
 		#returns a child node
 		child_state = list(self.state)
 		child_state[empty_index], child_state[empty_index + swap_index] = child_state[empty_index + swap_index], child_state[empty_index]
-		child_node = Node(child_state, self)
+		child_node = Node(child_state, self, self.cost + 1)
 		return child_node
 
 	def print_node(self):
@@ -19,6 +22,7 @@ class Node:
 			print self.state[3 * y + 2] if self.state[3 * y + 2] != 0 else " "
 		print ""
 
+	#Returns an expansion of nodes. Checks the path for duplicates and removes them
 	def expand(self):
 		expansion = []
 		empty_slot = self.state.index(0)
@@ -30,34 +34,89 @@ class Node:
 			expansion.append(self.__makeChildNode(-1, empty_slot))
 		if empty_slot != 2 and empty_slot != 5 and empty_slot != 8:
 			expansion.append(self.__makeChildNode(1, empty_slot))
+
+		nodes_to_delete = []
+		for x in expansion:
+			prev = self
+			while prev is not None:
+				if prev.state == x.state:
+					nodes_to_delete.append(x)
+					break
+				prev = prev.parent
+
+		for x in nodes_to_delete:
+			expansion.remove(x)
 		return expansion
 
-	def get_path():
-		return None
+	def get_path(self):
+		path = []
+		node = self
+		while node is not None:
+			path.append(node)
+			node = node.parent
+		return reversed(path)
 
-def tree_search(initial_node, goal):
-	fringe = initial_node.expand()
-	while True:
-		if len(fringe) == 0:
-			return -1
-		node = fringe.pop()
-		node.print_node()
-		if node == goal:
-			return node
-		fringe.extend(node.expand())
-
-start = [8,7,6,5,4,3,2,1,0]
+start = [8,6,7,2,5,4,3,0,1]
+#start = [0,1,3,4,2,5,7,8,6]
+#start = [1,2,3,4,5,6,7,0,8]
 goal = [1,2,3,4,5,6,7,8,0]
 
 start_node = Node(start)
+goal_node = Node(goal)
+
+#calculate the manhattan distance between two Nodes
+def manhattan_dist(comp1, comp2):
+	manhattan_distance = 0
+	for x in comp1.state:
+		if x == 0:
+			continue
+		linear_distance = int(math.fabs(comp2.state.index(x) - comp1.state.index(x)))
+		manhattan_distance += linear_distance / 3 + linear_distance % 3
+	return manhattan_distance
+
+def dist_from_goal(node):
+	return manhattan_dist(node, goal_node)
+
+
+def tree_search(initial_node, goal):
+	lowest = 20
+	fringe = initial_node.expand()
+	node_dict = {}	#dictionary to keep track of nodes
+	for x in fringe:
+		node_dict[tuple(x.state)] = x	
+
+	while True:
+		if len(fringe) == 0:
+			return -1
+		node = fringe.pop(0)
+		del node_dict[tuple(node.state)]
+		#node.print_node()
+		if node.state == goal.state:
+			return node
+		expansion = node.expand()
+		for x in expansion:
+			x_tuple = tuple(x.state)
+			if x_tuple in node_dict:
+				if node_dict[x_tuple].cost > x.cost:
+					node_dict[x_tuple] = x
+					fringe.remove(node_dict[x_tuple])
+					fringe.append(x)
+			else:
+				node_dict[x_tuple] = x
+				fringe.append(x)	
+		
+		#fringe.sort(key=dist_from_goal)
+
 for x in start_node.expand():
 	x.print_node()
 
-#goal = tree_search(Node(start), Node(goal))
+goal = tree_search(Node(start), Node(goal))
 
-"""if goal == -1:
+if goal == -1:
 	print "no solution"
 else:
-	goal.print_node();"""
+	print "solution found in :"
+	for x in goal.get_path():
+		x.print_node()
 
 
