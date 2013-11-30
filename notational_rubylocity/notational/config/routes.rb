d66b0b057677cf60b2caf49103e3e6cbfc0ed82e
Jiaqi Wu <wujiaqi@Jiaqis-MacBook-Pro-3.local>
2013-11-29 22:29:17 -0600

    Responsive UI final project implementation convert to Ruby on Rails

--- notational_rubylocity/notational/config/routes.rb
+++ notational_rubylocity/notational/config/routes.rb
@@ -1,4 +1,6 @@
 Notational::Application.routes.draw do
+  get "notational/index"
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -51,7 +53,7 @@ Notational::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => "welcome#index"
+  root :to => "notational#index"
 
   # See how all your routes lay out with "rake routes"
 
